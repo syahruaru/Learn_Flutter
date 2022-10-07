fa531504aabb718974f461a5e44a36ca8c0bb8b8
syahruaru <57306432+syahruaru@users.noreply.github.com>
2022-10-07 19:01:35 +0700

    Add files via upload
    Day 1

---
+++ main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+// The main function is the starting point for all our flutter apps.
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+      backgroundColor: Colors.blueGrey[600],
+      appBar: AppBar(
+        title: Center(child: Text('I Am Rich')),
+        backgroundColor: Colors.blueGrey[900],
+      ),
+      body: Center(
+          child: Image(
+        image: AssetImage('images/diamond.png'),
+      )),
+    ),
+  ));
+}
